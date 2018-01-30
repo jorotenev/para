527cc88fd862b1a363a882cdde740bf8430652d3
Georgi Tenev <georgetenevbg@gmail.com>
2018-01-30 10:38:38 +0200

    update the user model

--- app/models/user-model.ts
+++ app/models/user-model.ts
@@ -3,52 +3,30 @@ import {Observable} from "data/observable";
 export interface IUser {
     email: string,
     password: string;
-    name: string;
 }
-export class User extends Observable implements IUser {
 
-    private _email: string = "";
-    private _password: string = "";
-    private _name: string = "";
+export class User implements IUser {
 
 
-    constructor(email?: string, password?: string, name?: string) {
-        super();
-        this._email = email;
-        this._password = password;
-        this._name = name;
+    constructor(public email: string, private _password: string) {
     }
 
-    public get email() {
-        return this._email
+    static makeEmptyUserModel(): IUser {
+        return {
+            password: null,
+            email: null,
+        }
     }
 
-    public set email(email: string) {
-        this._email = email;
+    get password() {
+        return this._password
     }
 
-    public get password() {
-        return this._password;
-    }
-
-    public set password(pass: string) {
-        this._password = pass;
-    }
-
-    public set name(name: string) {
-        this._name = name;
-    }
-
-    public get name() {
-        return this._name;
+    set password(pwd: string) {
+        this._password = pwd;
     }
 
     public login() {
-        console.log(
-            `Username ${this._name}, 
-            user email ${this._email}
-            user pass ${this._password}`
-        );
     };
 
     public register() {
