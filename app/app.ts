798a62e1a9632afd67d1970cfc2313808b1a2b53
Georgi Tenev <georgetenevbg@gmail.com>
2018-01-30 13:30:54 +0200

    update the firebase init

--- app/app.ts
+++ app/app.ts
@@ -3,31 +3,17 @@ import * as app from 'application';
 import firebase = require("nativescript-plugin-firebase");
 import {readableTimestamp} from "~/utils/time";
 import {expenseAmountToString} from "~/utils/money";
-import {viewLogIn, viewAfterLogIn} from '~/app_config';
+import {viewLogIn, viewAfterLogIn, setFirebaseAuthToken} from '~/app_config';
 import {numberConverter} from "~/utils/number"
 import {navigateTo} from "~/utils/nav";
 
-firebase.init({
-    onAuthStateChanged: function (data) { // optional but useful to immediately re-logon the user when he re-visits your app
-        console.log(data.loggedIn ? "Logged in to firebase" : "Logged out from firebase");
-        if (data.loggedIn) {
-            console.log("in app.ts.  user's email address: " + (data.user.email ? data.user.email : "N/A"));
-            navigateTo(viewAfterLogIn)
-        } else {
-            console.log("in app.ts. user is not logged in")
-        }
-
-        firebase.getAuthToken({}).then(
-            function (token) {
-                console.log("Auth token retrieved: " + token);
-            },
-            function (errorMessage) {
-                console.log("Auth token retrieval error: " + errorMessage);
-            }
-        );
-    }
-});
-
+firebase.init();
+// firebase.getAuthToken({}).then(
+//     function (token) {
+//         console.log("Auth token retrieved: " + token);
+//         setFirebaseAuthToken(token)
+//     }
+// );
 // https://docs.nativescript.org/core-concepts/data-binding#example-7-adding-converters-in-the-application-module-resources
 app.setResources({
     'numberConverter': numberConverter,
--- app/app_config.ts
+++ app/app_config.ts
@@ -9,17 +9,48 @@ import {
     remove,
     clear
 } from "application-settings";
+import {getCurrencies} from "~/utils/money";
 
+const currencies = getCurrencies()
 
-let userPreferredCurrencyKey = 'user_preferred_currency_code';
+function validateCurrency(currency) {
+    if (Object.keys(currencies).indexOf(currency) === -1) {
+        throw new Error("Invalid default currency")
+    }
+}
 
+export const userPreferredCurrencyKey = 'user_preferred_currency_code_key';
+export const firebaseAuthTokenKey = 'usr_firebase_authtoken_key';
+const userFirebaseUIDKey = 'usr_uid_key';
+export const defaultCurrency = "EUR";
 
+validateCurrency(defaultCurrency)
+
+export function setCurrency(currency: string): boolean {
+    try {
+        validateCurrency(currency)
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
+export function setUserFirebaseUID(uid) {
+    setString(userFirebaseUID, uid)
+}
+
+export function setFirebaseAuthToken(token) {
+    setString(firebaseAuthTokenKey, token)
+}
 
 /*
  Config values
  */
-export const viewAfterLogIn = 'expense/add/add-expense';
-// export const viewAfterLogIn='expense/list/list';
-export const viewLogIn = 'auth/login-view';
+// export const viewAfterLogIn = 'expense/add/add-expense';
+export const viewAfterLogIn = 'expense/list/list';
+export const viewLogIn = 'auth/login/login-view';
 export const userPreferredCurrency = getString(userPreferredCurrencyKey, "EUR");
 export const apiAddress = "http://10.0.2.2:5000/"; //TODO hack
+export const userFirebaseUID = getString(userFirebaseUIDKey, null);
+export const firebaseAuthToken = getString(firebaseAuthTokenKey, null);
+
