9df9763d79ac8e9731da778ad82c1539a98ec6c1
Georgi Tenev <georgetenevbg@gmail.com>
2018-01-30 10:42:56 +0200

    small refactors

--- app/components/expense-form/expense-form.ts
+++ app/components/expense-form/expense-form.ts
@@ -1,4 +1,2 @@
-import {group_1, group_2, group_3} from "~/expense/common/common";
-
 export function loadedRad(args) {
 }
--- app/utils/ui.ts
+++ app/utils/ui.ts
@@ -1,6 +1,7 @@
 import {ActivityIndicator} from "tns-core-modules/ui/activity-indicator";
 
 export function toggleActivityIndicator(component:ActivityIndicator, state: boolean) {
+    //todo refactor - can include this logic in the XML and use an observable
     component.busy = state;
     component.visibility = state ? 'visible' : 'collapse'
 }
