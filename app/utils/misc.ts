63db674ba66563fdbe47d5e2e2fd8e6ea44096ed
Georgi Tenev <georgetenevbg@gmail.com>
2018-01-30 10:41:15 +0200

    forgot to add the observableproperty decorator

--- app/utils/misc.ts
+++ app/utils/misc.ts
@@ -1,3 +1,5 @@
+import {Observable} from "tns-core-modules/data/observable";
+
 export function hashCode(str) {
     let hash = 0;
     for (let i = 0; i < str.length; i++) {
@@ -6,4 +8,30 @@ export function hashCode(str) {
         hash = hash & hash; // Convert to 32bit integer
     }
     return hash;
+}
+
+//https://www.nativescript.org/blog/nativescript-observable-magic-string-property-name-be-gone
+export function ObservableProperty() {
+    return (target: Observable, propertyKey: string) => {
+        Object.defineProperty(target, propertyKey, {
+            get: function () {
+                return this["_" + propertyKey];
+            },
+            set: function (value) {
+                if (this["_" + propertyKey] === value) {
+                    return;
+                }
+
+                this["_" + propertyKey] = value;
+                this.notify({
+                    eventName: Observable.propertyChangeEvent,
+                    propertyName: propertyKey,
+                    object: this,
+                    value,
+                });
+            },
+            enumerable: true,
+            configurable: true
+        });
+    };
 }
