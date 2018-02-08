9064dbfd8ef77fc7500c4c6124259517ac1431f0
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-08 09:51:55 +0200

    use http method enum in the auth facade

--- app/api_facade/auth_facade.ts
+++ app/api_facade/auth_facade.ts
@@ -1,5 +1,6 @@
 import {Utils} from "./common";
 import {apiAddress, apiVersion} from "~/app_config";
+import {HTTPMethod} from "~/api_facade/db_facade";
 
 export const AUTH_API_ENDPOINT = `${apiAddress}auth_api/${apiVersion}/`;
 
@@ -11,7 +12,7 @@ export class AuthFacade implements IAuthFacade {
     static readonly POSTRegisterEndpoint = `${AUTH_API_ENDPOINT}register`;
 
     register(): Promise<void> {
-        return Utils.makeRequest(AuthFacade.POSTRegisterEndpoint, 'POST')
+        return Utils.makeRequest(AuthFacade.POSTRegisterEndpoint, HTTPMethod.POST)
     }
 
 }
