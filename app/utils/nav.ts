77f2e0834e9d9aff48cf0a6aac8bf3cd1ce66794
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-28 08:45:55 +0200

    expose extra param for the navigateTo()

--- app/utils/nav.ts
+++ app/utils/nav.ts
@@ -1,13 +1,13 @@
 let frameModule = require("tns-core-modules/ui/frame");
 
-export function navigateTo(path: string, clearHistory: boolean = false) {
+export function navigateTo(path: string, clearHistory: boolean = false, fromDrawer=true) {
     console.log("navigating to " + path);
     let topmost = frameModule.topmost();
     topmost.navigate({
         moduleName: path,
         clearHistory: clearHistory,
         context: {
-            fromDrawer: true
+            fromDrawer: fromDrawer
         }
     });
 }
